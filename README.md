7b2894846c9309fb0aa76ebfe4fc1ac86ef3b68f
Josh Duffney <jduffney@paylocity.com>
2019-10-12 07:36:12 -0500

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Kubernetes-Update-Apps-The-Hard-Way
 Exercise in manually updating applications running on Kubernetes
+
+1. Build and upload images to docker hub with tags
+2. Create kubernetes manifests for pods, replication sets and service
---
+++ v2/app/main.py
@@ -0,0 +1,13 @@
+import socket
+hostname = (socket.gethostname())
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "This is v2 running on pod " + hostname
+
+if __name__ == "__main__":
+    # Only for debugging while developing
+    app.run(host='0.0.0.0', debug=True, port=80)
